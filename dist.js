8d56c1077b980c548b8b422d148138664a3fde03
Cyrille Lebeaupin <clebeaupin@noop.fr>
2016-07-29 10:57:52 +0200

    Create windows installer using NSIS

--- dist.js
+++ dist.js
@@ -1,8 +1,23 @@
 import builder, {Platform} from "electron-builder";
+import os from "os";
+
+var targets = null;
+
+switch (os.platform()) {
+  case "linux":
+    targets =Platform.LINUX.createTarget(["deb"]);
+    break;
+  case "win32":
+    targets = Platform.WINDOWS.createTarget(["nsis"]);
+    break;
+  case "darwin":
+    targets = Platform.MAC.createTarget(["dev"]);
+    break;
+}
 
 // Promise is returned
 builder.build({
-  targets: Platform.LINUX.createTarget(["deb"]),
+  targets: targets,
   devMetadata: {
     directories: {
       "app": "build",
@@ -11,15 +26,21 @@ builder.build({
     build: {
       "appId": "readium.electron",
       "app-category-type": "ebook",
-      "npmRebuild": false
+      "npmRebuild": false,
+      "win": {
+        "icon": "assets/win/app.ico"
+      },
+      "nsis": {
+        "oneClick": false
+      }
     }
   }
 })
   .then(() => {
-    console.log("Done");
+    console.log("## Done");
     // handle result
   })
   .catch((error) => {
-    console.error("error");
+    console.error("## error", error);
     // handle error
   })
